1f320480d188e6d841e8f17196eaa2a02ebbc07e
Your Name <you@example.com>
2026-04-02 21:23:08 +0530

    wrote the logic to identify the type of unix machine

--- install.sh
+++ install.sh
@@ -28,6 +28,33 @@
 # Get information about the system
 ###################################
 
+# get the uname first and then `cat /etc/os-release`
+OS_VAR="$(uname)" #this variable will be either Linux or Darwin
+DISTRO=''
+
+#shopts
+set -euo pipefail
+
+getDistro() {
+    if [[ -r /etc/os-release ]]; then
+        DISTRO=$(grep -i '^NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
+    fi
+}
+
+if [[ "$OS_VAR" == "Darwin" ]]; then
+    printf 'Detected OS is Darwin\n'
+    DISTRO='Mac'
+    printf '%s\n' "$DISTRO"
+
+elif [[ "$OS_VAR" == "Linux" ]]; then
+    printf 'Detected OS is Linux...\n'
+    getDistro
+    printf '%s\n' "$DISTRO"
+
+fi
+
+#📐 CHECK:- works on fedora arch ubuntu
+
 #########STEP-II#############################################
 # Based on the system i need to download the depending files
 #############################################################
