54803b7b6ce6cb2251b50c32c377ac0c0c1b4ff7
Your Name <you@example.com>
2026-04-02 19:31:21 +0530

    Made the basic approach of the project

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
+#!/usr/bin bash
 
+#echo "Hello World"
+
+#########STEP-I####################
+# Get information about the system
+###################################
+
+#########STEP-II#############################################
+# Based on the system i need to download the depending files
+#############################################################
+
+#########STEP-III###################
+# Place them on to the specific part
+####################################
