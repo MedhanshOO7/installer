7ce934b62bfa6f95234f1160717299aea10b9dc6
Your Name <you@example.com>
2026-04-02 19:38:18 +0530

    Decided all the features of the script

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/usr/bin bash
 
+############################################################
+#dotfiles i need to add to a system
+# -> before anything I do i need to install dependencies
+# -> zsh configs
+# -> kitty configs (Mac/linux)
+# -> fastfetch configs
+# -> obsidian configs
+# -> .p10k.zsh
+# -> neovim
+# -> vim configs
+# -> vs-code
+# -> special kde setup
+# -> installing basic tools (something like a tui)
+############################################################
+##
+
 #echo "Hello World"
 
 #########STEP-I####################
