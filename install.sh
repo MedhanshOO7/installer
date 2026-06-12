fced37c9ed62219cb1d66d83a78a9e957202eed4
MedhanshOO7 <Medhansh07@outlook.com>
2026-06-12 16:47:46 +0530

    Fix for fedora

--- install.sh
+++ install.sh
@@ -559,7 +559,8 @@ ubuntuEza(){
 }
 
 fedoraBase(){
-    sudo dnf groupinstall -y "Development Tools"
+    sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf group install -y "Development Tools"
 }
 
 install_yay() {
